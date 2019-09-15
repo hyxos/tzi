a83729830e6e3fedc670c27b836e4016eb016528
Donnie Hicks <dw@1hx.ca>
2019-09-14 22:44:21 -0400

    refactored, fixed duo

--- functions.js
+++ functions.js
@@ -1,15 +1,15 @@
 const { elements, animals, range, Tau } = require('./constants')
 
 const mod = n => i => i % n
-const seniority = n => n % 60 !== 0 ? Math.abs(mod(60)(n)) : 60
-const elementOrder = n => n % 10 !== 0 ? Math.ceil(mod(10)(seniority(n)) / 2) : 5
+const seniority = n => mod(60)(n) !== 0 ? Math.abs(mod(60)(n)) : 60
+const elementOrder = n => mod(10)(n) !== 0 ? Math.ceil(mod(10)(seniority(n)) / 2) : 5
 const element = n => elements[elementOrder(n) - 1]
-const order = n => n % 12 !== 0 ? mod(12)(seniority(n)) : 12
+const order = n => mod(12)(n) !== 0 ? mod(12)(seniority(n)) : 12
 const animal = n => animals[order(n) - 1]
 const name = n => element(n) + " " + animal(n)
 const polarity = n => mod(2)(n)
 const opposite = n => seniority(n + 30)
-const duo = n => n % 2 === 0 ? n - 1 : n + 1
+const duo = n => mod(2)(seniority(n)) === 0 ? seniority(n) - 1 : seniority(n) + 1
 const trine = (n, i) => order(n) === order(i) + 4 || order(n) === order(i) - 4 
 // const square = (n, i, j) =>
 const yin = n => !Boolean(polarity(n))
