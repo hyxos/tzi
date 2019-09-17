4b2337549a7a2a17f6b5384d7cab5130136438f3
Donnie Hicks <dw@1hx.ca>
2019-09-17 16:29:49 -0400

    changing index.js into export module

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const Tzi = require('./tzi')
+const tzi = require('./functions')
 
-let woodRat = new Tzi(1)
-let woodRabbit = new Tzi(52)
-let woodRooster = new Tzi(22)
-console.log(woodRabbit.isSquare(woodRat))
-console.log(woodRooster.isSquare(woodRat))
-console.log(woodRooster.isSquare(25))
-
-let negDeck = Tzi.rangeArray(-60, -1).map(i => new Tzi(i))
-console.log(negDeck)
+module.exports = {
+  Tzi: Tzi,
+  tzi: tzi
+}
