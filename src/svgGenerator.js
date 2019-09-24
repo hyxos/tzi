38c7f8db67ab3cde1d2ed5002688eb809dfbc7b5
Donnie Hicks <dw@1hx.ca>
2019-09-23 21:11:02 -0400

    fixed bar naming, added some classes to svgs

--- src/svgGenerator.js
+++ src/svgGenerator.js
@@ -1,4 +1,10 @@
-import { name, column, row, hexColor, elementOrder, polarity } from './functions'
+import { 
+  urlName, 
+  column, 
+  row, 
+  hexColor, 
+  elementOrder, 
+  polarity } from './functions'
 
 const svgWrapper = 
 ({
@@ -10,21 +16,22 @@ const svgWrapper =
   strokeWidth=1,
   viewBox="-60 -60 120 120",
   dimensions=[100, 100],
-  anchor=[-60, -60]
+  anchor=[-60, -60],
+  className=""
 }) =>
 `<svg id="${id}" xmlns="http://www.w3.org/2000/svg" viewBox="${viewBox}" width="${dimensions[0]}" height="${dimensions[1]}">
-  <rect x="${anchor[0]}" y="${anchor[1]}" width="100%" height="100%" stroke="${stroke}" stroke-width="${strokeWidth}" fill="${boxFill}"/>
-  <g fill="${contentFill}">
+  <rect class="box" x="${anchor[0]}" y="${anchor[1]}" width="100%" height="100%" stroke="${stroke}" stroke-width="${strokeWidth}" fill="${boxFill}"/>
+  <g class="${className}" fill="${contentFill}">
     ${content}
   </g>
 </svg>`
 
-const hb1 = `<rect id="hb1"  x="-46" y="-50" rx="3" width="12" height="100" />`
-const hb2 = `<rect id="hb2"  x="-6" y="-50" rx="3" width="12" height="100" />`
-const hb3 = `<rect id="hb3" x="34" y="-50" rx="3" width="12" height="100" />`
-const vb1 = `<rect id="vb1" x="-50" y="-46" rx="3" width="100" height="12" />`
-const vb2 = `<rect id="vb2" x="-50" y="-6" rx="3" width="100" height="12" />`
-const vb3 = `<rect id="vb3" x="-50" y="34" rx="3" width="100" height="12" />`
+const vb1 = `<rect id="hb1"  x="-46" y="-50" rx="3" width="12" height="100" />`
+const vb2 = `<rect id="hb2"  x="-6" y="-50" rx="3" width="12" height="100" />`
+const vb3 = `<rect id="hb3" x="34" y="-50" rx="3" width="12" height="100" />`
+const hb1 = `<rect id="vb1" x="-50" y="-46" rx="3" width="100" height="12" />`
+const hb2 = `<rect id="vb2" x="-50" y="-6" rx="3" width="100" height="12" />`
+const hb3 = `<rect id="vb3" x="-50" y="34" rx="3" width="100" height="12" />`
 const dc1r1 = `<circle id="dc1r1" cx="-20" cy="-20" r="6" />`
 const dc2r1 = `<circle id="dc2r1" cx="20" cy="-20" r="6" />`
 const dc1r2 = `<circle id="dc1r2" cx="-20" cy="20" r="6" />`
@@ -36,24 +43,24 @@ const genbars = n => {
   let bars = ''
   switch(c) {
     case 1:
-      bars += hb2
+      bars += vb2
       break
     case 2:
-      bars += hb1 + '\n  ' + hb3
+      bars += vb1 + '\n  ' + vb3
       break
     case 3:
-      bars +=  hb1 + '\n  ' + hb2 + '\n  ' + hb3
+      bars +=  vb1 + '\n  ' + vb2 + '\n  ' + vb3
       break
   }
   switch(r) {
     case 2:
-      bars += vb2
+      bars += hb2
       break
     case 3:
-      bars += vb1 + '\n  ' + vb3
+      bars += hb1 + '\n  ' + hb3
       break
     case 4:
-      bars += vb1 + '\n  ' + vb2 + '\n  ' + vb3
+      bars += hb1 + '\n  ' + hb2 + '\n  ' + hb3
       break
   }
   return bars
@@ -80,16 +87,19 @@ const genDots = n => {
 }
 
 export const genGlyph = n => {
+  let className = "glyph"
   if (!polarity(n)) return svgWrapper({
-    id: name(n),
+    id: urlName(n),
     content: genbars(n) + '\n  ' + genDots(n),
     boxFill: hexColor(n),
-    contentFill: "white"
+    contentFill: "white",
+    className
   })
   else return svgWrapper({
-    id: name(n),
+    id: urlName(n),
     content: genbars(n) + '\n  ' + genDots(n),
-    contentFill: hexColor(n)
+    contentFill: hexColor(n),
+    className
   })
 }
 
